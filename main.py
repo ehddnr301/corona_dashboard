a1918505a0b7f8f12a1e84701a5d87c3889ee419
ehddnr301 <dy950328@gmail.com>
2020-08-22 21:32:26 +0900

    Total case count of each country

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
-daily_df = pd.read_csv('data/daily_report.csv')
-daily_df = daily_df[['Confirmed', 'Deaths', 'Recovered']].sum().reset_index(name='count')
-daily_df = daily_df.rename(columns={'index': 'condition'})
+daily_df = pd.read_csv("data/daily_report.csv")
+
+total_df = (
+    daily_df[["Confirmed", "Deaths", "Recovered"]].sum().reset_index(name="count")
+)
+total_df = total_df.rename(columns={"index": "condition"})
+
+countries_df = daily_df[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
+countries_df = countries_df.groupby("Country_Region").sum().reset_index()
+
