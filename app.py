8a35be3a3f78fe3b53917bd68d753a27a6adb592
ehddnr301 <dy950328@gmail.com>
2021-06-27 15:52:05 +0900

    reset css and font & layout tutorial

--- app.py
+++ app.py
@@ -3,34 +3,33 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
-import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+stylesheets = [
+    'https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css',
+    'https://fonts.googleapis.com/css2?family=Jua&display=swap'
+]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=stylesheets)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
-])
+app.layout = html.Div(
+    style={
+        'minHeight': '100vh',
+        'backgroundColor':'#111',
+        'color':'white',
+        'fontFamily': "Jua"
+    },
+    children = [
+        html.Header(
+            style={
+                'textAlign': 'center',
+                "paddingTop": "50px"
+            },
+            children=[
+                html.H1('Corona Dashboard', style= {"fontSize":50})
+            ]
+        )
+    ]
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
