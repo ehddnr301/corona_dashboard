899970700fdae9452a244fab77a7c14a89581b87
ehddnr301 <dy950328@gmail.com>
2021-06-27 16:47:23 +0900

    how to make table

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
+from builder import make_table
+from data import countries_df
 
 stylesheets = [
     'https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css',
@@ -26,6 +28,15 @@ app.layout = html.Div(
             },
             children=[
                 html.H1('Corona Dashboard', style= {"fontSize":50})
+            ]
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        make_table(countries_df)
+                    ]
+                )
             ]
         )
     ]
